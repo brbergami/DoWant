31b06ad918173e23c33b24eb2ab523b487ad8eaf
Bruno Bergami <brbergami@gmail.com>
2024-08-17 21:27:33 -0300

    Initial Commit

---
+++ DoWant/DoWantApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoWantApp.swift
+//  DoWant
+//
+//  Created by Bruno Bergami on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DoWantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DoWantTests/DoWantTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoWantTests.swift
+//  DoWantTests
+//
+//  Created by Bruno Bergami on 17/08/2024.
+//
+
+import Testing
+@testable import DoWant
+
+struct DoWantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
